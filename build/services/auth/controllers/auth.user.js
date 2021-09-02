f2769eca74c054d5dc713ae7f73aba5d5e77f6fc
ybalaev1 <balaev.ya@gazelkin.ru>
2021-09-02 12:03:37 +0300

    .

--- build/services/auth/controllers/auth.user.js
+++ build/services/auth/controllers/auth.user.js
@@ -59,6 +59,7 @@ var login = function (req, res, next) {
 };
 exports.login = login;
 var validJWTNeeded = function (req, res, next) {
+    console.log(req.headers);
     if (req.headers.authorization) {
         try {
             var auth = req.headers.authorization.split(' ');
