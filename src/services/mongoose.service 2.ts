327819c0f5cb46b96539f921105ee14adea5ec83
balaev.yr <balaev.yr@alidi.ru>
2023-07-01 20:17:11 +0500

    test

--- src/services/mongoose.service 2.ts
+++ src/services/mongoose.service 2.ts
@@ -6,7 +6,11 @@ const connectToDatabase = async (port?: number): Promise<void> => {
   const options: ConnectionOptions = {
     useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true,
   };
-  await mongoose.connect(`mongodb+srv://y1balaev:rVsueR6Q4UYwG6pb@cluster0.tozhfgs.mongodb.net/testingDB?retryWrites=true&w=majority`, options);
+  try {
+    await mongoose.connect(`mongodb+srv://y1balaev:rVsueR6Q4UYwG6pb@cluster0.tozhfgs.mongodb.net/testingDB?retryWrites=true&w=majority`, options);
+  } catch (error) {
+    console.log('connectToDatabase', error);
+  }
 
   // if (port === 3000) {
   //   await mongoose.connect(`mongodb+srv://y1balaev:rVsueR6Q4UYwG6pb@cluster0.tozhfgs.mongodb.net/${process.env.db_env}DB?retryWrites=true&w=majority`, options);
